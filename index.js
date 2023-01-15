cf4c64b074dc9906c5efa59eb9e3a5a487cd9cb1
Rishabh Bose <Rishabhbose3@gmail.com>
2023-01-15 13:13:09 +0530

    hello cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
     origin: [
       "*",
       "http://192.168.1.100:3000",
-      "https://thekamodaresort.com/",
+      "https://thekamodaresort.com",
       "https://thekamodaresort.com/admin",
     ],
 
