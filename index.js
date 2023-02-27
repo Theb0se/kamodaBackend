f3662ce66e3881c4ccd49c8c1d30b7c8c72e739b
Rishabh Bose <Rishabhbose3@gmail.com>
2023-02-27 11:04:19 +0530

    feedback cors fxd2

--- index.js
+++ index.js
@@ -28,16 +28,7 @@ app.use(
   })
 );
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
